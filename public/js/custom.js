70f35dbba6cbe3661ae3b350b6e3a1f90367dd7a
nicoldness <nicolgadingan@gmail.com>
2022-06-12 22:47:43 +0800

    Dev 1.0.99
    TNS108
    - Fixed missing menu after resize of screen.

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
-    var screen_width    =   window.screen.width;
-    
-    // Mobile Screen
-    if (screen_width >= 320 &&
-        screen_width <= 480) {
-        // $("#sidebar-admin").attr("hidden", true);
-        // $("#db-ticket-chart canvas").attr("hidden", true);
-    } else {
-        // $("#sidebar-admin").attr("hidden", false);
-        // $("#db-ticket-chart canvas").attr("hidden", false);
-    }
-
     $("body").on("click", "#mobile-menu", function () {
-        console.log("Menu Clicked");
         $("#sidebar-menu").toggle("show");
+    });
+
+    $(window).resize(function() {
+        var screen_width    =   window.screen.width;
+        if (screen_width >= 480) {
+            $("#sidebar-menu").show();    
+        } else {
+            $("#sidebar-menu").hide();
+        }
     });
 });
