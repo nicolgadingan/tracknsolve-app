e508208b1ea0493fb33a1a687a3028753a12df92
nicoldness <nicolgadingan@gmail.com>
2022-06-19 13:26:39 +0800

    Dev 1.1.10
    TNS118
    - Updated sidebar palette

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 
+    <link rel="stylesheet" href="{{ asset('css/tnstheme.css') }}">
+
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.6.0.js" crossorigin="anonymous"
         integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="></script>
